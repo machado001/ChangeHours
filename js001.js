7dbd390a88a83c3cd64b82b53909dac82eb6f28e
machado001 <machadowg@gmail.com>
2022-07-02 22:25:58 -0300

    refact

--- js001.js
+++ js001.js
@@ -1,6 +1,7 @@
 const showDate = new Date()
-const showOnlyHours = showDate.getHours()
+const showOnlyHours = 8
 const showOnlyMinutes = showDate.getMinutes()
+
 let insertSpan = function () {
     return setInterval(() => document.querySelector('span').innerHTML = `${showOnlyHours}:${showOnlyMinutes}`, 1000)
 }
@@ -17,17 +18,20 @@ const c = () => {
 
     } else if (showOnlyHours => 18 && showOnlyHours <= 24) {
         //condição para estar de noite
-        showHTML('#2d2929', 'A note chegou!', 'https://criticalhits.com.br/?attachment_id=208749')
+        showHTML('#2d2929', 'A noite chegou!', 'https://criticalhits.com.br/?attachment_id=208749')
 
     } else if (showOnlyHours >= 0 && showOnlyHours <= 6) {
         //condição para estar de madrugada
-        a('#2d2626', 'Madruga, vai ficar até tarde mais uma vez?', 'https://static.wixstatic.com/media/1f8f2a_e63789c40dbd4d05999ccdeaa222ccda~mv2.png/v1/fit/w_720%2Ch_480%2Cal_c/file.png')
+        showHTML('#2d2626', 'Madruga, vai ficar até tarde mais uma vez?', 'https://static.wixstatic.com/media/1f8f2a_e63789c40dbd4d05999ccdeaa222ccda~mv2.png/v1/fit/w_720%2Ch_480%2Cal_c/file.png')
     }
 }
 document.addEventListener('DOMContentLoaded', c)
 
 function showHTML(color, text, img) {
     setBGColor = document.querySelector('body').style.backgroundColor = color
-    showParagraph = document.querySelector('#result').innerHTML = text
+    showParagraph = setTimeout(() => {
+        document.querySelector('#result').innerHTML = text
+    },2000) 
     setImage = document.querySelector('img').src = img
+    return;
 }
