c2f69ba2341348b5d852a147eb3316158edd8000
machado001 <machadowg@gmail.com>
2022-07-06 23:56:44 -0300

    little review

--- js001.js
+++ js001.js
@@ -1,32 +1,34 @@
 const showDate = new Date()
-const showOnlyHours = showDate.getHours()
-const showOnlyMinutes = showDate.getMinutes()
+const hours = showDate.getHours()
+const minutes = showDate.getMinutes()
+console.log(minutes)
 
 let insertSpan = function () {
-    const num = 5;
     const span = document.querySelector('span')
     const inner = setInterval(() =>
-        span.innerHTML = num < 10 ? `${showOnlyHours}:0${num}` : `${showOnlyHours}:${num}`, 1000)
+        span.innerHTML = minutes < 10 ? `${hours}:0${minutes}` : `${hours}:${minutes}`, 1)
     return inner;
 }
-window.addEventListener('DOMContentLoaded', insertSpan)
+document.addEventListener('DOMContentLoaded', insertSpan)
 
 const c = () => {
-    if (showOnlyHours >= 6 && showOnlyHours <= 12) {
+    if (hours >= 6 && hours <= 12) {
         //condição para estar de manhã
         showHTML('#0fd2e2', 'Bom dia!', 'https://www.colunatech.com.br/wp-content/uploads/2021/08/one-peace-1.jpg')
 
-    } else if (showOnlyHours > 12 && showOnlyHours < 18) {
+    } else if (hours > 12 && hours < 18) {
         //condição para estar de tarde
         showHTML('#e6a60f', 'Tardezinha boa!', 'https://criticalhits.com.br/wp-content/uploads/2021/06/0896cd12e66c175047dea1f0aba7d182-910x512.jpg')
 
-    } else if (showOnlyHours => 18 && showOnlyHours <= 24) {
+    } else if (hours => 18 && hours <= 24) {
         //condição para estar de noite
         showHTML('#2d2929', 'A noite chegou!', 'https://criticalhits.com.br/?attachment_id=208749')
+        console.log('lol???')
 
-    } else if (showOnlyHours >= 0 && showOnlyHours <= 6) {
+    } else  {
         //condição para estar de madrugada
         showHTML('#2d2626', 'Madruga, vai ficar até tarde mais uma vez?', 'https://static.wixstatic.com/media/1f8f2a_e63789c40dbd4d05999ccdeaa222ccda~mv2.png/v1/fit/w_720%2Ch_480%2Cal_c/file.png')
+        console.log('lol?')
     }
 }
 document.addEventListener('DOMContentLoaded', c)
@@ -35,7 +37,7 @@ function showHTML(color, text, img) {
     setBGColor = document.querySelector('body').style.backgroundColor = color
     showParagraph = setTimeout(() => {
         document.querySelector('#result').innerHTML = text
-    }, 2000)
+    }, 1)
     setImage = document.querySelector('img').src = img
     return;
 }
